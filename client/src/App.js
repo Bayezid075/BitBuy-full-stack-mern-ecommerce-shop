421c8f9815706517974014ea8be0555bfbd18080
Bayezid <mrbayezidhoshen@gmail.com>
2022-09-18 19:08:57 +0600

    React UI Design done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Cart from "./Pages/Cart";
 import Login from "./Pages/Login";
 import Product from "./Pages/Product";
-// import Register from "./Pages/Register";
+// import Register from "./Pages/Register";/
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
 
